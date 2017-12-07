4c2055b0500a912167e103bd4f88fb3721560112
Christopher Harrop <bannastre@gmail.com>
2017-12-07 15:42:03 +0000

    Docking Station is initialised with an empty list of bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,11 @@
 require_relative 'bike'
 
-class DockingStation 
+class DockingStation
+  
+  def initialize
+    @docked_bikes = []
+  end
+  
   def say_hello
     'Hello World!'
   end
@@ -8,4 +13,9 @@ class DockingStation
   def release_bike
     Bike.new
   end
+
+  def show_docked_bikes
+    @docked_bikes
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,6 +7,8 @@ describe DockingStation do
 
   it { is_expected.to respond_to(:release_bike) }
 
+  it { is_expected.to respond_to(:show_docked_bikes) }
+
   it 'can release a new bike' do
     expect(subject.release_bike.working?).to eql(true)
   end
@@ -15,5 +17,8 @@ describe DockingStation do
     expect(subject.release_bike).to be_a(Bike)
   end
 
-  
+  it 'knows the bikes that are docked' do
+    expect(subject.show_docked_bikes).to eql([])
+  end
+
 end
